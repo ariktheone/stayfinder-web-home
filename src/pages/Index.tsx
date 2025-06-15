3935a4bceec42320588503759dc1406d02419789
Arijit Mondal <arijitmondal200430@gmail.com>
2025-06-15 16:56:20 +0000

    Fix build errors in Index page
    Fixes various syntax and structural errors in `src/pages/Index.tsx` to resolve build issues.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -97,7 +97,7 @@ const Index = () => {
       <section className="relative py-24 px-4 overflow-hidden">
         {/* Background Pattern */}
         <div className="absolute inset-0 bg-gradient-to-r from-rose-50/30 to-blue-50/30"></div>
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%23f1f5f9" fill-opacity="0.3"%3E%3Ccircle cx="7" cy="7" r="1"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-40"></div>
+        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%23f1f5f9\" fill-opacity=\"0.3\"%3E%3Ccircle cx=\"7\" cy=\"7\" r=\"1\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-40"></div>
         
         <div className="container mx-auto text-center relative z-10">
           <div className="animate-fade-in">
